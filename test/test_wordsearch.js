5d478acd06f4410b5ade4af27a3ce96dd6be3d42
Sebastian <lynchseb@gmail.com>
2020-11-04 22:21:59 +0000

    implemented wordSearch and test_wordSearch.js

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -16,6 +16,7 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'FRANK')
+    
 
     assert.isFalse(result);
   });
